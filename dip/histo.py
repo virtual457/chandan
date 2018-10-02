e08b71ee5b0e18bfa110dd3ef5d33ecfffb89532
chandan.keelara <chandan.keelara@gmail.com>
2018-10-03 01:32:03 +0530

    modified

---
+++ dip/histo.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+arr=plt.imread("blow.jpg")
+size=arr.shape
+c=[]
+res=[]
+b=[]
+a=np.array(range(256))
+a.fill(0)
+for i in arr:
+	print(a.shape)
+	for j in i:
+		u=int(j[0])
+		m=a[u]
+		n=m+1
+		a[u]=n
+for i in a:
+	b.append(i/(size[0]*size[1]))		
+print(sum(b))
+c.append(b[0])
+for i in range(1,255):
+	c.append((b[i]+c[i-1]))
+
+p=0
+o=[]
+for i in c:
+	o.append(int(i*255))
+print(o)
+for i in arr:
+	temp=[]
+	for j in i: 		
+		d=c[j[0]]	
+		temp.append([d,d,d])
+	res.append(temp)
+print("gamma correction over")
+res1=np.array(res)
+plt.imsave("result.png",arr=res1)
