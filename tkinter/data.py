225df60f71414e8764838fe869f353e906abd11b
chandan.keelara <chandan.keelara@gmail.com>
2018-10-14 00:20:52 +0530

    added tkinter

---
+++ tkinter/data.py
@@ -0,0 +1,29 @@
+from Tkinter import *
+master=Tk()
+master.title("Some RANdom Db")
+def insert():
+	print("this is function insert")
+def show():
+	print("this as function show")
+def dele():
+	print("this is function delete")
+def first():
+	print("this is function first")
+def prev():
+	print("this is function prev")
+def nest():
+	print("this is function next")
+def last():
+	print("this is function last")
+text1=Label(master,text="whatever u want")
+e1=Entry(master).grid(row=1,column=0)
+e2=Entry(master).grid(row=2,column=0)
+Button(master,text="INSERT",command=insert).grid(row=1,column=5)
+Button(master,text="SHOW",command=show ).grid(row=2,column=5)
+Button(master,text="DELE",command=dele).grid(row=3,column=5)
+Button(master,text="FIRST",command=first).grid(row=5,column=1)
+Button(master,text="PREV",command=prev).grid(row=5,column=2)
+Button(master,text="NEXT",command=nest).grid(row=5,column=3)
+Button(master,text="LAST",command=last).grid(row=5,column=4)
+Button(master,text="QUIT",command=master.quit).grid(row=4,column=5)
+mainloop()
